9f173ccd9e2cb3da2440f3007cf29b2bda719882
Dannyblue12 <156196390+Dannyblue12@users.noreply.github.com>
2026-01-22 13:49:33 +0100

    Refactor PracticingJavaApplication class formatting

--- src/main/java/com/demojava/practicingJava/PracticingJavaApplication.java
+++ src/main/java/com/demojava/practicingJava/PracticingJavaApplication.java
@@ -2,13 +2,12 @@ package com.demojava.practicingJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; 
 
-// The 'exclude' part below guarantees NO login screen, even if a dependency sneaks in.
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class PracticingJavaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PracticingJavaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PracticingJavaApplication.class, args);
+	}
+
 }
